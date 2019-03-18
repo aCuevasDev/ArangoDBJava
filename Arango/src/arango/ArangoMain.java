8abc9491a607fe31e41192940bef5595235c947f
Alex <alexcuevitas@gmail.com>
2019-03-18 22:26:39 +0100

    I find this so ugly it needs a commit on its own.

--- Arango/src/arango/ArangoMain.java
+++ Arango/src/arango/ArangoMain.java
@@ -9,36 +9,41 @@ import model.Empleado;
 
 public class ArangoMain {
 
+	// Cuevas: No me gusta nada tener estos mÈtodos aquÌ, yo los pondrÌa en el
+	// Controller.
+
 	public static void main(String[] args) {
 		insertarEmpleado();
 		crearDepartamento();
 	}
-	
+
 	private static void insertarEmpleado() {
-		System.out.println(Controller.getInstance().crearEmpleado(new Empleado("zzz", "zzz", "zz", "zzz", new Departamento())));
+		System.out.println(
+				Controller.getInstance().crearEmpleado(new Empleado("zzz", "zzz", "zz", "zzz", new Departamento())));
 	}
 
 	public static void login() {
 		String username = InputAsker.askString("Introduce tu username");
 		String contrasenya = InputAsker.askString("Introduce tu contrasenya");
-		if(Controller.getInstance().login(username, contrasenya)) {
+		if (Controller.getInstance().login(username, contrasenya)) {
 			System.out.println("Bienvenido");
-		}else {
+		} else {
 			System.out.println("Usuario o contrase√±a incorrectos");
 		}
 	}
-	
+
 	private static void crearDepartamento() {
 		Departamento newDepartamento = new Departamento();
-        newDepartamento.setNombre(InputAsker.askString("Introduce el nombre del departamento: "));
-        // TODO buscar solo los empleados sin departamento??
-        List<Empleado> empleados = Controller.getInstance().getAllUsers();
-        int eleccion = 	InputAsker.askElementList("Escoje al jefe", empleados);
-        newDepartamento.setJefe(eleccion == 0 ? null : empleados.get(eleccion - 1));
-        Set<Empleado> empleadosDelDepartamento = InputAsker.generarListaApartirDeListaSinRepetidos("Introduce el empleado a a√±adir a la lista", empleados, Empleado.class);
-        newDepartamento.setEmpleados(empleadosDelDepartamento);
-        Controller.getInstance().crearDepartamento(newDepartamento);
-        System.out.println("Departamento creado correctamente!!");
-        //TODO poner el empleado como que es jefe y a√±adir al departamento
+		newDepartamento.setNombre(InputAsker.askString("Introduce el nombre del departamento: "));
+		// TODO buscar solo los empleados sin departamento??
+		List<Empleado> empleados = Controller.getInstance().getAllUsers();
+		int eleccion = InputAsker.askElementList("Escoje al jefe", empleados);
+		newDepartamento.setJefe(eleccion == 0 ? null : empleados.get(eleccion - 1));
+		Set<Empleado> empleadosDelDepartamento = InputAsker.generarListaApartirDeListaSinRepetidos(
+				"Introduce el empleado a a√±adir a la lista", empleados, Empleado.class);
+		newDepartamento.setEmpleados(empleadosDelDepartamento);
+		Controller.getInstance().crearDepartamento(newDepartamento);
+		System.out.println("Departamento creado correctamente!!");
+		// TODO poner el empleado como que es jefe y a√±adir al departamento
 	}
 }

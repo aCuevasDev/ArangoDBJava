0f1276ae1673b3df0bbadd9979ce2318c9ab2c6a
alex <5alexbouoliete@gmail.com>
2019-03-30 16:44:34 +0100

    change server

--- Arango/src/persistence/ArangoUtils.java
+++ Arango/src/persistence/ArangoUtils.java
@@ -15,7 +15,7 @@ public abstract class ArangoUtils {
 	protected ArangoDB connection;
 
 	protected ArangoUtils() {
-		connection = new ArangoDB.Builder().host("172.16.2.50", 8529).password("stucom").build();
+		connection = new ArangoDB.Builder().host("alexraspberry.ddns.net", 8531).password("stucom").build();
 		db = connection.db("GuacamoleDB");
 	}
 

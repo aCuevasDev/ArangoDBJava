95daf1f41b4c90fbbdcbb5d09463e2180c2a56d8
Alex <alexcuevitas@gmail.com>
2019-03-30 18:27:24 +0100

    empleado

--- Arango/src/springboot/RESTController.java
+++ Arango/src/springboot/RESTController.java
@@ -1,15 +1,24 @@
 package springboot;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import controller.Controller;
+import model.EmpleadoDTO;
+
+@CrossOrigin
 @RestController
 @RequestMapping("/rest")
 public class RESTController {
+	
+	Controller controller = Controller.getInstance();
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	@RequestMapping("/empleado")
+	public List<EmpleadoDTO> index() {
+		return controller.getAllUsers();
 	}
 
 }
